574e35cd4eca1d2d18fe831bc05c2168318a5a55
Nathan Rajlich <nathan@tootallnate.net>
2010-01-15 10:20:18 -0800

    Modified 'sprite-follow-mouse' example to show off the engine a little bit, but is more hardware intensive as the result.

--- examples/sprite-follow-mouse/main.js
+++ examples/sprite-follow-mouse/main.js
@@ -24,48 +24,67 @@ SpriteFollow.CursorClass = Class.create(SGF.Sprite, {
             height: spriteset.spriteHeight,
             zIndex: 2
         });
-
-        SGF.Input.observe("mousedown", this.createDrop.bind(this));
+        
+        // Attach two different mouse events to this "cursor"
+        this.bindedCreateRect = this.createRect.bind(this);
+        SGF.Input
+            .observe("mousedown", this.bindedCreateRect)
+            .observe("mousemove", this.bindedCreateRect);
     },
     update: function() {
         this.x = SGF.Input.pointerX;
         this.y = SGF.Input.pointerY;
     },
-    createDrop: function() {
-        new SpriteFollow.Drop({
-            x: this.x,
-            y: this.y
-        });
+    createRect: function() {
+        //if (SpriteFollow.Rect.instances.size() < 50) {
+            new SpriteFollow.Rect({
+                x: this.x,
+                y: this.y
+            });
+        //}
     }
 });
 
-// A "drop" that gets created when the user clicks a mouse key. It just falls
+// A Rectangle that gets created when the user clicks a mouse key. It just falls
 // down off the screen, and fades away as it falls. An instance is removed from
 // the game loop when it becomes completely invisible.
-SpriteFollow.Drop = Class.create(SGF.Rectangle, {
+SpriteFollow.Rect = Class.create(SGF.Rectangle, {
     initialize: function($super, options) {
+        var size = ((Math.random()*18)+2);
         $super(Object.extend({
-            // Make it move down the screen at a random speed (between 1 and 10)
-            dy: (Math.random()*9)+1,
+            // Make it move across the screen at a random speed (between -10 and 10)
+            dy: ((Math.random()*19)+1) - 10,
+            dx: ((Math.random()*19)+1) - 10,
+
+            width: size,
+            height: size,
+
             // Appear below the CursorClass
             zIndex: 1,
+            
             // Generate some random color
             color: (Math.random()*255).round().toColorPart() +
                    (Math.random()*255).round().toColorPart() +
                    (Math.random()*255).round().toColorPart()
         }, options));
         this.startY = this.bottom();
+        this.constructor.instances.push(this);
         SGF.Game.current.addComponent(this);
     },
     update: function($super) {
-        this.opacity = (SGF.Screen.height - this.bottom()) / (SGF.Screen.height - this.startY);
+        this.opacity -= .01;
         this.rotation += 10;
         if (this.opacity <= 0) {
-            SGF.Game.current.removeComponent(this);
+            this.remove();
         }
         $super();
+    },
+    remove: function() {
+        SGF.Game.current.removeComponent(this);
+        this.constructor.instances = this.constructor.instances.without(this);
     }
 });
+SpriteFollow.Rect.instances = [];
 
 // Make a new instance of our CursorClass class, for use in our game.
 SpriteFollow.cursor = new SpriteFollow.CursorClass();
