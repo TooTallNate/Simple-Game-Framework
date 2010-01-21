2584e89fc3110f39da1dc2c59d98a10f940d224a
Nathan Rajlich <nathan@tootallnate.net>
2010-01-21 09:12:29 -0800

    Added a 'template

---
+++ templates/html/helpers.rb
@@ -0,0 +1,19 @@
+module PDoc
+  module Generators
+    module Html
+      module Helpers
+        module BaseHelper
+        end
+        
+        module LinkHelper
+        end
+        
+        module CodeHelper
+        end
+        
+        module MenuHelper
+        end
+      end
+    end
+  end
+end
