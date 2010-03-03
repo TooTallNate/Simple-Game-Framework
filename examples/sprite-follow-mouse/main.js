0a3905d0be83c2364271378ed2cace1eec4bc038
Nathan Rajlich <nathan@tootallnate.net>
2010-03-03 12:44:09 -0800

    Finalized "sprite-follow-mouse" (hopefully).

--- examples/sprite-follow-mouse/main.js
+++ examples/sprite-follow-mouse/main.js
@@ -11,7 +11,7 @@
  */
 
 
-// Set the background color.
+// Set the background color, in RRGGBB notation.
 SGF.Screen.color = "123456";
 
 // Hide the native mouse cursor. This is for custom game mouse cursors
@@ -19,7 +19,7 @@ SGF.Screen.color = "123456";
 // Note that all mouse movement and button events are still fired.
 SGF.Screen.useNativeCursor(false);
 
-// Firstly, it's always a good idea to namespace you games. This is because
+// Firstly, it's always a good idea to namespace your games. This is because
 // we don't like polluting the global namespace, since you could potentially
 // overwrite important native objects.
 SpriteFollow = {};
@@ -40,8 +40,11 @@ SpriteFollow.CursorClass = Class.create(SGF.Sprite, {
         
         // Attach various input events to this "cursor" instance
         this.bindedCreateRect = this.createRect.bind(this);
-        SGF.Input.observe("mousedown", this.bindedCreateRect)
-                 .observe("mousemove", this.bindedCreateRect);
+        SGF.Input.observe("mousedown",  this.bindedCreateRect)
+                 .observe("mouseup",    this.bindedCreateRect)
+                 .observe("mousemove",  this.bindedCreateRect)
+                 .observe("keydown",    this.bindedCreateRect)
+                 .observe("keyup",      this.bindedCreateRect);
     },
     update: function() {
         // Update the Sprite's location to the location of the native cursor
@@ -57,41 +60,50 @@ SpriteFollow.CursorClass = Class.create(SGF.Sprite, {
     }
 });
 
-// A Rectangle that gets created when the user clicks a mouse key. It just falls
-// down off the screen, and fades away as it falls. An instance is removed from
-// the game loop when it becomes completely invisible.
+// A Rectangle that gets created when the user performs various input events.
+// The Rectangle starts at the position of the mouse, and moves away at a
+// random speed, and rotates at a random rotation.
 SpriteFollow.Rect = Class.create(SGF.Rectangle, {
     initialize: function($super, options) {
         
         // We'll make it be a square, with a random w/h between 2 and 20.
-        var size = ((Math.random()*18)+2);
+        var size = (Math.random()*18) + 2;
 
         $super(Object.extend({
-            // Make it move across the screen at a random speed (between -10 and 10)
-            dy: (Math.random()*20) - 10,
-            dx: (Math.random()*20) - 10,
-
             width: size,
             height: size,
+            
+            opacity: .5,
 
             // Appear below the CursorClass
             zIndex: 1,
             
-            // Generate some random color
+            // Generate a random color, utilizing Prototype's Number#toColorPart()
             color: (Math.random()*255).round().toColorPart() +
                    (Math.random()*255).round().toColorPart() +
                    (Math.random()*255).round().toColorPart()
         }, options));
+        
+        // Make it move across the screen at a random speed (between -10 and 10)
+        this.dy = (Math.random()*20) - 10;
+        this.dx = (Math.random()*20) - 10;
+
+        // Make a random rotation speed between 0° and 40°
+        this.rotationSpeed = Math.random() * (Math.PI/180 * 40);
 
         // Add this new instance to the game loop.
         SGF.Game.current.addComponent(this);
+        this.startCount = SGF.Game.current.updateCount;
     },
-    update: function($super) {
-        $super();
-        this.opacity -= .01;
-        this.rotation += .1;
-        if (this.opacity <= 0) {
+    update: function(updateCount) {
+        // First check if it should be removed because it's lived it's life
+        if (updateCount - this.startCount > 50) {
             this.remove();
+        } else { // Otherwise update the Rect appropriately.
+            this.x += this.dx;
+            this.y += this.dy;
+            this.opacity -= .01;
+            this.rotation += this.rotationSpeed;
         }
     },
     remove: function() {
